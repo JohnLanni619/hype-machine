cb7fe12d72f545b4d9cc831c289cdf3841c1d89b
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-09-21 10:00:58 -0400

    added signup button, removed the link about form, and changed Me to Us!

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -38,8 +38,7 @@ function ContactForm() {
 
   return (
     <section>
-      <Link to="/contact">← Contact Us</Link>
-      <h1 data-testid="h1tag">Contact me</h1>
+      <h1 data-testid="h1tag">Contact Us</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -59,6 +58,7 @@ function ContactForm() {
           </div>
         )}
         <button data-testid="button" type="submit">Submit</button>
+        <Link to="/signup"><button type="button">Sign-up</button></Link>
       </form>
     </section>
   );
