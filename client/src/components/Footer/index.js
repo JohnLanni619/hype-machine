e6885c4a2954398d3763c9ee3dc7a2fdb3196a12
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-09-20 21:37:50 -0400

    added contact form, helper, and link to contact us in the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import "../../index.css"
+import { Link } from 'react-router-dom';
 const Footer = () => {
     return  (
         <footer>
             <ul className="footer-container">
-                <li>Contact Us</li>
+                <li><Link to="/contact">Contact Us</Link></li>
                 <li>Group 5 &copy; 2021</li>
             </ul>
         </footer>
@@ -11,3 +12,4 @@ const Footer = () => {
 };
 
 export default Footer
+
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth.js';
 
@@ -31,7 +30,7 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      
 
       <h2>Login</h2>
       <form onSubmit={handleFormSubmit}>
