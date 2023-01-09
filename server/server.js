1c003b781a714cd87a98522d0912a31adead0c9d
LEVERAGE-101322\John Lanni <john@leveragegc.com>
2023-01-09 14:58:19 -0500

    updated server config

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
