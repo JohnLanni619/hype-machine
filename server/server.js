1861437ced76ff26d9805276f5ca83f9d7f1e715
John Lanni <johnlanni619@gmail.com>
2026-03-12 20:27:31 -0400

    Add dotenv dependency and update static asset paths in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
+require('dotenv').config();
 
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
@@ -29,15 +30,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV ?? 'development' !== 'development') {
   console.log('not in development')
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 } else {
   console.log('in development')
 }
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 db.once('open', () => {
